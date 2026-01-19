52d2bf9f25401124fb2a0e6ebcbd3a0b32027971
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-01-20 00:55:22 +0530

    Add files via upload

---
+++ Problems_on_Numbers/program_17.py
@@ -0,0 +1,8 @@
+# Accept on character from user and check whether that character is vowel (a,e,i,o,u) or not.
+
+ch = input("Enter the character : ")
+
+if(ch == 'a' or ch == 'A' or ch == 'e' or ch =='E' or ch == 'i' or ch == 'I' or ch == 'o' or ch == 'O' or ch == 'u' or ch == 'U'):
+    print("The given charcter is vowel")
+else:
+    print("The given charcter is not vowel")
