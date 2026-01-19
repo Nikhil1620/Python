076ee5ae7624c2685e74f983e52e8d85a103c480
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-01-20 00:44:13 +0530

    Add files via upload

---
+++ Problems_on_Numbers/program6.py
@@ -0,0 +1,14 @@
+# Mathematical Operations of two by giving user's input
+
+iNo1 = int(input("Enter the First number : "))
+iNo2 = int(input("Enter the Second number : "))
+
+Addition = iNo1 + iNo2
+Substraction = iNo1 - iNo2
+Multiplication = iNo1 * iNo2
+Division = iNo1 // iNo2
+
+print("Addition :",Addition)
+print("Substraction :",Substraction)
+print("Multiplication :",Multiplication)
+print("Division :",Division)
