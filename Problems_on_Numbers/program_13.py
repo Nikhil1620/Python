1b4294095f2010bd3b545595a8afd1962d14bb8d
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-01-20 00:54:09 +0530

    Add files via upload

---
+++ Problems_on_Numbers/program_13.py
@@ -0,0 +1,8 @@
+# Accept number from user and check whether number is even or odd.
+
+iNo = int(input("Enter the number :"))
+
+if(iNo % 2 == 0):
+    print("The given number is even")
+else:
+    print("The given number is Odd")
