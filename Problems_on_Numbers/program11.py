798e9823a1a51219244d8f6ef6e48e9d00e52a99
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-01-20 00:46:18 +0530

    Delete Problems_on_Numbers/program11.py

--- Problems_on_Numbers/program11.py
+++
@@ -1,8 +0,0 @@
-#Accept on number from user if number is less than 10 then print “Hello” otherwise print “Demo”. 
-
-iNo = int(input("Enter the number : "))
-
-if(iNo < 10):
-    print("Hello")
-else:
-    print("Demo")
