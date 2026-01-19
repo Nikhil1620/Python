3e460d9cd1457f01c347cebc1cbaa3ddf3f7117f
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-01-20 00:45:05 +0530

    Add files via upload

---
+++ Problems_on_Numbers/program9.py
@@ -0,0 +1,8 @@
+#Accept one number and check whether is is divisible by 5 or not.
+
+iNo = int(input("Enter the number : "))
+
+if(iNo % 5 == 0):
+    print("The given number is divisible by 5.")
+else:
+    print("The given number is not divisible by 5.")
