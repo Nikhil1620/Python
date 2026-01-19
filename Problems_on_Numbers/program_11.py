f5147308d39ca6399d2e1bec940fc67f04d3cd77
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-01-20 00:53:33 +0530

    Add files via upload

---
+++ Problems_on_Numbers/program_11.py
@@ -0,0 +1,8 @@
+#Accept on number from user if number is less than 10 then print “Hello” otherwise print “Demo”. 
+
+iNo = int(input("Enter the number : "))
+
+if(iNo < 10):
+    print("Hello")
+else:
+    print("Demo")
