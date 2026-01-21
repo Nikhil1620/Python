0e63a74b2fc65d61a1ae2eaa2db7e7b020d7c3bc
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-01-21 17:51:01 +0530

    Add files via upload

---
+++ Problems_on_Numbers/program_30.py
@@ -0,0 +1,10 @@
+# Write a program which accept two numbers and check whether numbers are
+# equal or not. 
+
+iNo1 = int(input("Enter the First number : "))
+iNo2 = int(input("Enter the Second number : "))
+
+if(iNo1 == iNo2):
+    print("The given numbers are same.")
+else:
+    print("The given numbers are not same.")
