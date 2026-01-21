7b85a0f2ae0d0646a09977d932ab789a02540ef5
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-01-21 17:50:41 +0530

    Add files via upload

---
+++ Problems_on_Numbers/program_29.py
@@ -0,0 +1,9 @@
+# Write a program which accept one number from user and check whether that
+# number is greater than 100 or not.
+
+iNo = int(input("Enter the number : "))
+
+if(iNo < 100):
+    print("Smaller")
+else:
+    print("Greater")
