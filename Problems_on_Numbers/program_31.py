0ed992b452636f52fcd7b3b8048f57f5250bd3be
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-01-21 17:51:21 +0530

    Add files via upload

---
+++ Problems_on_Numbers/program_31.py
@@ -0,0 +1,10 @@
+# Write a program which accept three numbers and print its multiplication. 
+
+iNo1 = int(input("Enter the first number : "))
+iNo2 = int(input("Enter the seconf number : "))
+iNo3 = int(input("Enter the third number : "))
+
+print(iNo1,"*",iNo2,"*",iNo3,":")
+iMultiplication = iNo1 * iNo2 * iNo3
+print(iMultiplication)
+
