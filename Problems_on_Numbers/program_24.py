95e8a2f83db871ec51e476077bb65cac57b47e22
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-01-21 17:49:15 +0530

    Add files via upload

---
+++ Problems_on_Numbers/program_24.py
@@ -0,0 +1,9 @@
+# Find Maximum of Two Numbers 
+
+iNo1 = int(input("Enter the first number : "))
+iNo2 = int(input("Enter the Second numner : "))
+
+if(iNo1 > iNo2):
+    print(iNo1, "is maximum number")
+else:
+    print(iNo2,"is maximum number")
