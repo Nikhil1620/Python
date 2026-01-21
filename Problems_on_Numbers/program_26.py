3339fbb503456787de2480043c31df8dfa7c7347
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-01-21 17:49:49 +0530

    Add files via upload

---
+++ Problems_on_Numbers/program_26.py
@@ -0,0 +1,10 @@
+# Check Positive, Negative, or Zero 
+
+iNo = int(input("Enter the number : "))
+
+if(iNo > 0):
+    print(iNo,"is positive number")
+elif(iNo < 0):
+    print(iNo,"is negative number")
+else:
+    print(iNo,"is Zero")
