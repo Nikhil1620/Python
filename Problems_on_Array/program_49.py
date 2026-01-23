9d432de4a82213f2c199fb0bd8e8650aa59715c5
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-01-23 20:06:46 +0530

    Add files via upload

---
+++ Problems_on_Array/program_49.py
@@ -0,0 +1,21 @@
+# Accept N numbers from user and return frequency of 11 from it.
+
+iNo = int(input("Enter the number of elements : "))
+
+Arr = []
+
+iValue = 0
+
+for i in range(iNo):
+    iValue = int(input("Enter the element : "))
+    Arr.append(iValue)
+
+iNum = int(input("Enter the number : "))
+
+iFrequency = 0
+
+for num in Arr:
+    if(num == iNum):
+        iFrequency = iFrequency + 1
+
+print("The frequency of given number is ", iFrequency)
