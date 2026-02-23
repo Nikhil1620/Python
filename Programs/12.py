91fc5c5f64f9dc5ee1a08c48c6bad07a27a22311
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-02-23 23:54:21 +0530

    Add files via upload

---
+++ Programs/12.py
@@ -0,0 +1,9 @@
+
+Letter = (input("Enter the Character : "))
+
+Arr = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']
+
+if(Letter in Arr):
+    print(Letter,"is Vowel")
+else:
+    print(Letter,"is not Vowel")
