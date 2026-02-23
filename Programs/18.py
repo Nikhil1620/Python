069878b28f924eb02769ad3b044dc9cb41978bf8
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-02-23 23:55:53 +0530

    Add files via upload

---
+++ Programs/18.py
@@ -0,0 +1,7 @@
+iNo1 = int(input("Enter the first number : "))
+iNo2 = int(input("Enter the Second number : "))
+
+if(iNo1 > iNo2):
+    print(iNo1,"is maximum.")
+else:
+    print(iNo2,"is maximum.")
