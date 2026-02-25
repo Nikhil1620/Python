161b7856b332be62b6e7d11a8df32095cf7e934c
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-02-25 23:04:58 +0530

    Add files via upload

---
+++ Programs/26.py
@@ -0,0 +1,8 @@
+iMarks = int(input("Enter the obtained marks : "))
+iTotal = int(input("Enter the total marks : "))
+
+Percentage = (iMarks / iTotal)*100
+
+print("The percentage is",Percentage,"%")
+
+
