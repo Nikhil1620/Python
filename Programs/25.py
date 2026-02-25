75441ea916fa03f4df63d0d7912fcb181e24125d
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-02-25 23:04:39 +0530

    Add files via upload

---
+++ Programs/25.py
@@ -0,0 +1,7 @@
+iNo1 = int(input("Enter the first number : "))
+iNo2 = int(input("Enter the Second number : "))
+iNo3 = int(input("Enter the third number : "))
+
+iProduct = iNo1 * iNo2 * iNo3
+
+print("The product of the given number is",iProduct)
