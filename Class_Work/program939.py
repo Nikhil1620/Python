139caaa7368d8cc750d8555a1fd0aba8f2372914
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-04-05 08:47:08 +0530

    Add files via upload

---
+++ Class_Work/program939.py
@@ -0,0 +1,14 @@
+     
+def main():
+    print("Enter first number : ")
+    No1 = int(input())
+
+    print("Enter second number : ")
+    No2 = int(input())
+
+    if(No1 > No2):
+        print(No1,"is maximum")
+    else:
+        print(No2,"is maximum")
+
+main()
