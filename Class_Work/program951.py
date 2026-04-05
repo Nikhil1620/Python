ce637b1f75ae91ba3eee7ca6f18a5a1f0803a103
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-04-05 08:50:09 +0530

    Add files via upload

---
+++ Class_Work/program951.py
@@ -0,0 +1,17 @@
+
+def DisplayDigits(No):
+    iDigit = 0
+    iSum = 0
+
+    while(No != 0):
+        iDigit = No % 10
+        print(iDigit, end = " ")
+        No = No // 10    
+
+def main():
+    print("Enter the number : ")
+    Value = int(input())
+
+    DisplayDigits(Value)
+
+main()
