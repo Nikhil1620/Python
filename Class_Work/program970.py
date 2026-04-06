9298830b8ad49b0be6bafb829b3139ec580e6fb1
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-04-06 19:04:00 +0530

    Add files via upload

---
+++ Class_Work/program970.py
@@ -0,0 +1,21 @@
+
+def CopySmall(Brr):
+    Result = ""
+
+    for ch in Brr:
+        if(ch >= 'a' and ch <= 'z'):
+            Result = Result + ch
+
+    return Result
+
+def main():
+    print("Enter the string : ")
+    Arr = input()
+
+
+    Ret = CopySmall(Arr)
+
+    print("Updated string is ",Ret)
+    
+
+main()
